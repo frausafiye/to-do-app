7e51dc45d179514944c65a5f5d50534923a9a744
frausafiye <s_gulenc@outlook.com>
2020-11-16 10:13:03 +0100

    Updates

---
+++ precache-manifest.32e5f8387d5c6e93e3946e95a907801d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "064c1942c1e07904cbaeb717f7e12cb1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "53c722ca182b6536cb5e",
+    "url": "./static/css/main.5d67a9a3.chunk.css"
+  },
+  {
+    "revision": "1b7fd8f48e54f7bd85e3",
+    "url": "./static/js/2.de26d967.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.de26d967.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53c722ca182b6536cb5e",
+    "url": "./static/js/main.148ea179.chunk.js"
+  },
+  {
+    "revision": "8cdb25f3ffb41440c4d8",
+    "url": "./static/js/runtime-main.e6c4d886.js"
+  }
+]);
